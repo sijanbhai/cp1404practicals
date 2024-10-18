a04bbe1daa438ccfeb317dda84b30f680db446e5
Tanvir Sijan <mdtanvirhossain.sijan@my.jcu.edu.au>
2024-10-18 17:14:08 +1000

    import random to get random numbers

---
+++ prac_03/random.py
@@ -0,0 +1,13 @@
+"""
+#In line 1 I saw 5
+the smallest number can be 5 and largest number can be 20
+# in line 2 I saw 5
+Smallest number can 3 and largest number can be 9
+4 cant be produced
+#in line 3 I saw 3.013959580237302
+Smallest number can be 2.5 and largest number can be 5.5
+"""
+import random
+
+random_number = random.randint(1, 100)
+print(random_number)
