b054e13ef84a0aee8771c260cdab202ea7e053e5
Tanvir Sijan <mdtanvirhossain.sijan@my.jcu.edu.au>
2024-10-30 15:47:57 +1000

    task 2

--- prac_03/files.py
+++ prac_03/files.py
@@ -3,3 +3,9 @@ name = input("What is your name? ")
 file = open('name.txt', 'w')
 file.write(name)
 file.close()
+
+# Task 2
+file = open('name.txt', 'r')
+name_from_file = file.readline().strip()
+file.close()
+print(f"Hi {name_from_file}!")
