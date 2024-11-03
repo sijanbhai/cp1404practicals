cadc304c01c2ef511b7e265674dedc7b0baa9e1c
Tanvir Sijan <mdtanvirhossain.sijan@my.jcu.edu.au>
2024-11-03 20:14:03 +1000

    added new function

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -8,20 +8,25 @@ FILENAME = "subject_data.txt"
 
 def main():
     data = load_data()
-    print(data)
+    display_subject_details(data)
 
 
 def load_data():
     """Read data from file formatted like: subject, lecturer, number of students."""
     data_list = []  # Initialize an empty list to hold each line's data
-    input_file = open(FILENAME)
-    for line in input_file:
-        line = line.strip()  # Remove the \n
-        parts = line.split(',')  # Separate the data into its parts
-        parts[2] = int(parts[2])  # Convert the student number to an integer
-        data_list.append(parts)  # Add this line's data as a list to data_list
-    input_file.close()
+    with open(FILENAME) as input_file:
+        for line in input_file:
+            line = line.strip()  # Remove the \n
+            parts = line.split(',')  # Separate the data into its parts
+            parts[2] = int(parts[2])  # Convert the student number to an integer
+            data_list.append(parts)  # Add this line's data as a list to data_list
     return data_list  # Return the list of lists
+
+
+def display_subject_details(data):
+    """Display details of each subject in a formatted output."""
+    for subject, lecturer, student_count in data:
+        print(f"{subject} is taught by {lecturer} and has {student_count} students")
 
 
 main()
