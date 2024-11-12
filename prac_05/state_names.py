43c6c393eb97b4a2fe71e5924c0e02bdcf9f2300
Tanvir Sijan <mdtanvirhossain.sijan@my.jcu.edu.au>
2024-11-12 21:28:01 +1000

    a loop to print all states

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -8,7 +8,8 @@ File needs reformatting
 CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
                 "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania", "SA": "South Australia"}
 print(CODE_TO_NAME)
-
+for code, name in CODE_TO_NAME.items():
+    print(f"{code:3} is {name}")
 state_code = input("Enter short state: ").upper()
 while state_code != "":
     if state_code in CODE_TO_NAME:
