433d3397ed5c510f076788982bdfa2af6de2a76c
Tanvir Sijan <mdtanvirhossain.sijan@my.jcu.edu.au>
2024-12-06 02:53:50 +1000

    added button handler

--- prac_08/box_layout_demo.py
+++ prac_08/box_layout_demo.py
@@ -1,23 +1,20 @@
 from kivy.app import App
 from kivy.lang import Builder
-from kivy.uix.textinput import TextInput
 from kivy.uix.label import Label
-from kivy.uix.button import Button
 
 class BoxLayoutDemo(App):
     def build(self):
-        self.title = "Name Input and Greeting"
+        self.title = "Box Layout Demo"
         self.root = Builder.load_file('box_layout.kv')
         return self.root
 
-    def greet_user(self):
-        name = self.root.ids.name_input.text  # Get text from the TextInput widget
-        greeting = f"Hello, {name}!" if name else "Please enter your name."
-        self.root.ids.greeting_label.text = greeting  # Update the label with greeting
-
-    def clear_name(self):
-        self.root.ids.name_input.text = ""  # Clear the text in TextInput widget
-        self.root.ids.greeting_label.text = "Please enter your name."  # Reset the label
-
+    def handle_greet(self):
+        # Find the label by id and update the text
+        name_input = self.root.ids.name_input.text
+        greeting_label = self.root.ids.greeting_label
+        if name_input:
+            greeting_label.text = f"Hello, {name_input}!"
+        else:
+            greeting_label.text = "Please enter your name."
 
 BoxLayoutDemo().run()
