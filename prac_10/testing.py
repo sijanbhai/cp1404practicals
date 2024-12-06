2c90e8a99ae5a9c6457a43480e43f6f6185d56f0
Tanvir Sijan <mdtanvirhossain.sijan@my.jcu.edu.au>
2024-12-07 01:54:02 +1000

    todo 5

--- prac_10/testing.py
+++ prac_10/testing.py
@@ -9,8 +9,7 @@ from prac_06.car import Car
 
 def repeat_string(s, n):
     """Repeat string s, n times, with spaces in between."""
-    return s * n
-
+    return " ".join([s] * n)
 
 def is_long_word(word, length=5):
     """
@@ -25,9 +24,20 @@ def is_long_word(word, length=5):
     return len(word) >= length
 
 
-def repeat_string(s, n):
-    """Repeat string s, n times, with spaces in between."""
-    return " ".join([s] * n)
+def format_phrase(phrase):
+    """
+    Format a phrase as a sentence, starting with a capital letter and ending with a full stop.
+    >>> format_phrase("hello")
+    'Hello.'
+    >>> format_phrase("It is an ex parrot.")
+    'It is an ex parrot.'
+    >>> format_phrase("this is a test")
+    'This is a test.'
+    """
+    phrase = phrase.strip()  # Remove any leading/trailing spaces
+    if not phrase.endswith("."):
+        phrase += "."  # Ensure the phrase ends with a single full stop
+    return phrase[0].upper() + phrase[1:]  # Capitalize the first letter
 
 
 def run_tests():
@@ -46,16 +56,3 @@ def run_tests():
 
 run_tests()
 doctest.testmod()
-
-# TODO: 4. Fix the failing is_long_word function
-# (Don't change the tests, change the function!)
-
-# TODO: 5. Write and test a function to format a phrase as a sentence,
-# starting with a capital and ending with a single full stop.
-# Important: start with a function header and just use pass as the body
-# then add doctests for 3 tests:
-#   'hello' -> 'Hello.'
-#   'It is an ex parrot.' -> 'It is an ex parrot.'
-# and one more that you decide is a useful test.
-# Run your doctests and watch the tests fail.
-# Then write the body of the function so that the tests pass.
