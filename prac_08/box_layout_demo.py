aa3b1f0f10a02c1d9c9bd78c04ae03d0b1ae93a1
Tanvir Sijan <mdtanvirhossain.sijan@my.jcu.edu.au>
2024-12-06 14:13:48 +1000

    added id label

--- prac_08/box_layout_demo.py
+++ prac_08/box_layout_demo.py
@@ -1,6 +1,5 @@
 from kivy.app import App
 from kivy.lang import Builder
-from kivy.uix.label import Label
 
 class BoxLayoutDemo(App):
     def build(self):
@@ -8,14 +7,16 @@ class BoxLayoutDemo(App):
         self.root = Builder.load_file('box_layout.kv')
         return self.root
 
+    def handle_clear(self):
+        # Clear any name input or reset text
+        self.root.ids.output_label.text = 'Enter your name'
+
     def handle_greet(self):
-        # Find the label by id and update the text
-        name_input = self.root.ids.name_input.text
-        greeting_label = self.root.ids.greeting_label
-        if name_input:
-            greeting_label.text = f"Hello, {name_input}!"
+        # Change the label's text to greet the user
+        name = self.root.ids.name_input.text  # Get the text from the name input field
+        if name:  # If the name is not empty
+            self.root.ids.output_label.text = f'Hello, {name}!'  # Greet the user
         else:
-            greeting_label.text = "Please enter your name."
-        print('greet')
+            self.root.ids.output_label.text = 'Hello, stranger!'  # Default greeting if name is empty
 
 BoxLayoutDemo().run()
